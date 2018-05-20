659703d6f40a21101cf39996e7213db31124f62c
Guest <Guest@192.168.0.15>
2018-05-19 18:52:43 -0600

    2018-05-19 Created Location classes

---
+++ PioneerTrail/src/pioneertrail/model/Location.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pioneertrail.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author Guest
+ */
+public class Location implements Serializable {
+    
+    private int column;
+    private int row;
+    private boolean visited;
+    private double amountRemaining;
+    private ArrayList<Map> maps = new ArrayList<>();
+
+    public Location() {
+    }
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public double getAmountRemaining() {
+        return amountRemaining;
+    }
+
+    public void setAmountRemaining(double amountRemaining) {
+        this.amountRemaining = amountRemaining;
+    }
+    public ArrayList<Map> getMap() {
+        return maps;
+    }
+
+    public void setGames(ArrayList<Map> maps) {
+        this.maps = maps;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + this.column;
+        hash = 73 * hash + this.row;
+        hash = 73 * hash + Objects.hashCode(this.visited);
+        hash = 73 * hash + (int) (Double.doubleToLongBits(this.amountRemaining) ^ (Double.doubleToLongBits(this.amountRemaining) >>> 32));
+        hash = 73 * hash + Objects.hashCode(this.maps);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "column=" + column + ", row=" + row + ", visited=" + visited + ", amountRemaining=" + amountRemaining + ", maps=" + maps + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.column != other.column) {
+            return false;
+        }
+        if (this.row != other.row) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.amountRemaining) != Double.doubleToLongBits(other.amountRemaining)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        return Objects.equals(this.maps, other.maps);
+    }
+
+    public void setVisited(String yes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
