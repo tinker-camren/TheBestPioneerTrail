d04afa3ebab4e25bf70a0ccae620d15bead4989e
Kayla17 <35283831+Kayla17@users.noreply.github.com>
2018-05-31 22:13:46 -0600

    5-31-2018 ToolControlTest tests

--- PioneerTrail/src/pioneertrail/control/ToolControl.java
+++ PioneerTrail/src/pioneertrail/control/ToolControl.java
@@ -5,10 +5,51 @@
  */
 package pioneertrail.control;
 
+import pioneertrail.model.InventoryItem;
+
 /**
  *
  * @author Tinkerc
  */
 public class ToolControl {
-    
+
+    //Danica
+    public static double chopWood(InventoryItem item, double scrap) {
+
+//pseudocode:
+        // BEGIN
+        //   IF (item.getCount < 1) THEN
+        //        RETURN -1
+        //
+        //   IF (item.getWeight < 1) THEN
+        //	      RETURN - 2 
+        //
+        //   IF (item.getCount > 15) THEN
+        //        RETURN -3
+        //
+        //   IF (item.getWeight > 12) THEN
+        //        RETURN -4   	
+        //
+        //   RETURN item.getCount * (item.getWeight – item.getWeight * scrap)
+        // END 
+        
+        if (item.getCount() < 1) { //is item count 0 or negative?
+            return -1;
+        }
+        
+        if (item.getWeight() < 1) { //is item weight 0 or negative?
+            return -2;
+        }
+        
+        if (item.getCount() > 15) { //is item count greater than 15?
+            return -3;
+        }
+        
+        if (item.getWeight() > 12) { //is item weight greater than 12?
+            return -4;
+        }
+        
+        return item.getCount() * (item.getWeight() - item.getWeight() * scrap);
+    }
+
 }
--- PioneerTrail/test/pioneertrail/control/ToolControlTest.java
+++ PioneerTrail/test/pioneertrail/control/ToolControlTest.java
@@ -9,28 +9,186 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import pioneertrail.model.InventoryItem;
 
 /**
  *
  * @author Danica
  */
 public class ToolControlTest {
-    
+
     public ToolControlTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
 
+   /*
+    * Test chopWood method of ToolControl
+    * Danica's Test
+    */
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void chopWood() {
+        System.out.println("chopWood Danica's Test");
+        
+        //Create object instances
+        InventoryItem item = new InventoryItem();
+        
+        //Test Case 1
+        System.out.println("\n\tTest case 1");
+        
+        //Define the input variables    
+        item.setCount(11);
+        item.setWeight(5);    
+        double scrap = 0.5;
+       
+        //Expected output
+        double expResult = 27.5;
+        
+        //Call method
+        double result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 2
+        System.out.println("\n\tTest case 2");
+        
+        //Define the input variables    
+        item.setCount(-2);
+        item.setWeight(10);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = -1;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 3
+        System.out.println("\n\tTest case 3");
+        
+        //Define the input variables    
+        item.setCount(9);
+        item.setWeight(-5);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = -2;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 4
+        System.out.println("\n\tTest case 4");
+        
+        //Define the input variables    
+        item.setCount(16);
+        item.setWeight(12);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = -3;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 5
+        System.out.println("\n\tTest case 5");
+        
+        //Define the input variables    
+        item.setCount(1);
+        item.setWeight(13);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = -4;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 6
+        System.out.println("\n\tTest case 6");
+        
+        //Define the input variables    
+        item.setCount(1);
+        item.setWeight(12);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = 6;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 7
+        System.out.println("\n\tTest case 7");
+        
+        //Define the input variables    
+        item.setCount(15);
+        item.setWeight(1);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = 7.5;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
+        
+        
+        //Test Case 8
+        System.out.println("\n\tTest case 8");
+        
+        //Define the input variables    
+        item.setCount(15);
+        item.setWeight(12);    
+        scrap = 0.5;
+       
+        //Expected output
+        expResult = 90;
+        
+        //Call method
+        result = ToolControl.chopWood(item, scrap);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tGathered Wood = " + result);
     }
-    
+
 }
