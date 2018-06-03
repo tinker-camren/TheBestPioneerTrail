fee5f5db895cf66544d17cb97880956d5c4b40a9
tinkerc24 <tin16004@byui.edu>
2018-06-02 19:10:50 -0600

    06/02/2018 - Added tests for repairWagon

--- PioneerTrail/test/pioneertrail/control/ToolControlTest.java
+++ PioneerTrail/test/pioneertrail/control/ToolControlTest.java
@@ -10,6 +10,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import pioneertrail.model.InventoryItem;
+import pioneertrail.model.Tool;
+import pioneertrail.model.Wagon;
 
 /**
  *
@@ -190,5 +192,243 @@ public class ToolControlTest {
         assertEquals(expResult, result, 0.0);
         System.out.println("\tGathered Wood = " + result);
     }
+
+    /**
+     * Test of repairWagon method, of class ToolControl.
+     * Camren's test
+     */
+    @Test
+    public void testRepairWagon() {
+        System.out.println("repairWagon" + "\nCamren's Assignment");
+        
+        //Create object instances
+        Wagon WAGON = new Wagon();
+        InventoryItem WOOD = new InventoryItem();
+        Tool HAMMER = new Tool();
+        
+        
+
+        //Test Case 1
+        System.out.println("\n\tTest case 1");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(10);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(80);
+        WAGON.setHealth(60);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        int expResult = 80;
+        
+        //Call method
+        int result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 2
+        System.out.println("\n\tTest case 2");
+        
+        //Define the input variables    
+        WOOD.setItemType("Water");
+        WOOD.setCount(12);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(60);
+        WAGON.setHealth(55);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = -1;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 3
+        System.out.println("\n\tTest case 3");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(11);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(0);
+        WAGON.setHealth(40);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = -2;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 4
+        System.out.println("\n\tTest case 4");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(0);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(40);
+        WAGON.setHealth(20);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = -3;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 5
+        System.out.println("\n\tTest case 5");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(5);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Gun");
+        HAMMER.setDurability(80);
+        WAGON.setHealth(75);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = -4;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 6
+        System.out.println("\n\tTest case 6");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(20);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(20);
+        WAGON.setHealth(80);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = 100;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 7
+        System.out.println("\n\tTest case 7");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(1);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(100);
+        WAGON.setHealth(80);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = 82;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 8
+        System.out.println("\n\tTest case 8");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(2);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(1);
+        WAGON.setHealth(70);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = 74;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 9
+        System.out.println("\n\tTest case 9");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(10);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(100);
+        WAGON.setHealth(0);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = 20;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+        
+        //Test Case 10
+        System.out.println("\n\tTest case 10");
+        
+        //Define the input variables    
+        WOOD.setItemType("Wood");
+        WOOD.setCount(20);   
+        HAMMER.setMultiplier(2);
+        HAMMER.setToolType("Hammer");
+        HAMMER.setDurability(40);
+        WAGON.setHealth(60);
+        WAGON.setMaxHealth(100);
+       
+        //Expected output
+        expResult = 100;
+        
+        //Call method
+        result = ToolControl.repairWagon(WOOD, HAMMER, WAGON);
+        
+        //compare expected result with returned result
+        assertEquals(expResult, result, 0.0);
+        System.out.println("\tNew Wagon Health = " + result);
+    }
+
+
 
 }
