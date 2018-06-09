1fce5f5069519ead92810a08d195b1daba5abee6
Kayla17 <35283831+Kayla17@users.noreply.github.com>
2018-06-08 20:31:23 -0600

    6-8-2018 Help Menu etc.

--- PioneerTrail/src/pioneertrail/view/StartProgramView.java
+++ PioneerTrail/src/pioneertrail/view/StartProgramView.java
@@ -39,27 +39,29 @@ public class StartProgramView {
         boolean valid = false;
         
         while (valid == false) {
-            System.out.println("The Pioneer Trail game is a text based role playing game. "
-                    + "In this game, you will be a pioneer family who leads a wagon train "
-                    + "preparing to go west. You have suffered constant religious persecution "
-                    + "and wish to find freedom and a new life in a better place. "
-                    + "You have heard of a place called Zion in the mountains that offers "
-                    + "such freedom and are eager to start your journey.\n"
-                    + "Your family is composed of both parents and three children. "
-                    + "Before starting the long journey of 1,500 miles, you must buy "
-                    + "a series of items necessary for the trip, such as: food, water, tools, "
-                    + "spare parts for the wagon, weapons, ammunition, etc. "
-                    + "You must plan carefully so that you will have the items you need "
-                    + "before you begin your journey.\n"
-                    + "Problems may arise during the trip, such as illness or death of family members, "
-                    + "snake bites, broken wagons, adverse weather conditions, etc. "
-                    + "You will have the opportunity to hunt if you have weapons and ammunition "
-                    + "when you spot a bison, or an elk. If you have the needed supplies"
-                    + " you could repair your damaged wagon, and fish when arriving at a lake or river. "
-                    + "You will also have to stop to rest from time to time, due to weak animals or "
-                    + "to obtain food. The game is over when at least two family members have reached Zion.");
+            System.out.println("\n*****************************"
+                    + "*******************************"
+                    + "*******************************"
+                    + "\nThe Pioneer Trail game is a text based role playing game. "
+                    + "\nIn this game, you will be a pioneer family who leads a wagon train preparing to go west."
+                    + "\nYou have suffered constant religious persecution and wish to find freedom and a new life "
+                    + "\nin a better place. You have heard of a place called Zion in the mountains that offers such "
+                    + "\nfreedom and are eager to start your journey. "
+                    + "\nYour family is composed of both parents and three children. Before starting the long journey "
+                    + "\nof 1,500 miles, you must buy a series of items necessary for the trip, such as: food, water, "
+                    + "\ntools, spare parts for the wagon, weapons, ammunition, etc. You must plan carefully so that "
+                    + "\nyou will have the items you need before you begin your journey. "
+                    + "\nProblems may arise during the trip, such as illness or death of family members, snake bites, "
+                    + "\nbroken wagons, adverse weather conditions, etc. You will have the opportunity to hunt if you "
+                    + "\nhave weapons and ammunition when you spot a bison, or an elk. If you have the needed supplies "
+                    + "\nyou could repair your damaged wagon, and fish when arriving at a lake or river. You will also "
+                    + "\nhave to stop to rest from time to time, due to weak animals or to obtain food. The game is over "
+                    + "\nwhen at least two family members have reached Zion."
+                    + "\n*******************************"
+                    + "*******************************"
+                    + "*******************************");
             
-            System.out.println("Please enter your name: ");
+            System.out.println("\nPlease enter your name: ");
             Scanner scanner = new Scanner(System.in);
             inputs[0] = scanner.nextLine();
             inputs[0] = inputs[0].trim();
@@ -88,10 +90,10 @@ public class StartProgramView {
             return false;
         }
         
-        System.out.println("================================================="
+        System.out.println("\n=========================================="
                 + "Welcome to the game " + playersName
                 + " We hope you have a lot of fun!"
-                + "=================================================");
+                + "===========================================");
 
         MainMenuView mainMenuView = new MainMenuView();
         mainMenuView.displayMainMenuView();
