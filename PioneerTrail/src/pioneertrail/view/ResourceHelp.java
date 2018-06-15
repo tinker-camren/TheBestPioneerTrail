6162f4bbf783e2c435a54e36cb423a586d373fb1
Kayla17 <35283831+Kayla17@users.noreply.github.com>
2018-06-14 22:37:02 -0600

    6-14-2018 Added dialogue to most help menu items

--- PioneerTrail/src/pioneertrail/view/ResourceHelp.java
+++ PioneerTrail/src/pioneertrail/view/ResourceHelp.java
@@ -12,7 +12,14 @@ package pioneertrail.view;
 class ResourceHelp {
 
     void displayResourceHelp() {
-        System.out.println("test resource help");
+        System.out.println(
+                "Resources can be used in a few different ways: "
+                + "\nFamily members will use food and water during "
+                + "\ntheir journey to Zion, and wood will be used "
+                + "\nwhen repairing the wagon. Resources may also "
+                + "\nbe required in certain location events. "
+                + "\nResources can be gathered by hunting, chopping "
+                + "\nwood, or triggering location events. ");
     }
     
 }
