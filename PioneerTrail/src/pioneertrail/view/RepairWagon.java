d5e69f81f3aab6ae24c9ba9013546e794f6fe0fd
Kayla17 <35283831+Kayla17@users.noreply.github.com>
2018-06-23 01:32:45 -0600

    6-23-2018 More view changes

--- PioneerTrail/src/pioneertrail/view/RepairWagon.java
+++ PioneerTrail/src/pioneertrail/view/RepairWagon.java
@@ -20,10 +20,11 @@ import pioneertrail.model.Wagon;
  */
 public class RepairWagon extends View {
 
-    @Override
-    public String[] getInputs() {
+    public RepairWagon() {
+        displayMessage = buildMenu();
+    }
 
-        String[] inputs = new String[1];
+    public String buildMenu() {
 
         ArrayList<InventoryItem> items = new ArrayList<InventoryItem>();
         Game game = PioneerTrail.getCurrentGame();
@@ -32,47 +33,92 @@ public class RepairWagon extends View {
         InventoryItem item = items.get(0);
         Tool tool = wagon.getTools().get(0);
 
-        System.out.println("**********"
+        String input = "\n**********"
                 + "\nRepair Wagon"
-                + "\n**********");
-        System.out.println("\nWagon Health: " + wagon.getHealth());
+                + "\n**********";
+        input += ("\n\nWagon Health: " + wagon.getHealth());
 
         if (tool.getToolType().equalsIgnoreCase("Hammer")) {
             if (tool.getDurability() > 0) {
-                System.out.println("Hammer Durability: " + tool.getDurability());
+                input += ("\nHammer Durability: " + tool.getDurability());
             } else {
-                System.out.println("Hammer broken or non-existent");
+                input += ("\nHammer broken or non-existent");
             }
         } else {
-            System.out.println("Hammer broken or non-existent");
+            input += ("\nHammer broken or non-existent");
         }
         if (item.getItemType().equalsIgnoreCase("Wood")) {
             if (item.getCount() > 0) {
-                System.out.println("Wood Amount: " + item.getCount());
+                input += ("\nWood Amount: " + item.getCount());
             } else {
-                System.out.println("No wood. You must gather wood before repairing"
+                input += ("\nNo wood. You must gather wood before repairing"
                         + " your wagon");
             }
         } else {
-            System.out.println("No wood. You must gather wood before repairing"
+            input += ("\nNo wood. You must gather wood before repairing"
                     + " your wagon");
         }
-        System.out.println("\nR - Repair Wagon "
+        input += ("\n\nR - Repair Wagon "
                 + "\nQ - Quit");
-        String input = this.getInput("Enter your selection: ");
-        inputs[0] = input;
+        input = this.getInput("Enter your selection: ");
+        input = input;
 
-        if (inputs[0].length() < 1) {
-            System.out.println("You must enter a non-blank value");
+        if (input.length() < 1) {
+            input += ("You must enter a non-blank value");
         }
 
-        return inputs;
+        return input;
+        
+//        String[] inputs = new String[1];
+//
+//        ArrayList<InventoryItem> items = new ArrayList<InventoryItem>();
+//        Game game = PioneerTrail.getCurrentGame();
+//        Wagon wagon = game.getWagon();
+//        items = wagon.getItems();
+//        InventoryItem item = items.get(0);
+//        Tool tool = wagon.getTools().get(0);
+//
+//        System.out.println("**********"
+//                + "\nRepair Wagon"
+//                + "\n**********");
+//        System.out.println("\nWagon Health: " + wagon.getHealth());
+//
+//        if (tool.getToolType().equalsIgnoreCase("Hammer")) {
+//            if (tool.getDurability() > 0) {
+//                System.out.println("Hammer Durability: " + tool.getDurability());
+//            } else {
+//                System.out.println("Hammer broken or non-existent");
+//            }
+//        } else {
+//            System.out.println("Hammer broken or non-existent");
+//        }
+//        if (item.getItemType().equalsIgnoreCase("Wood")) {
+//            if (item.getCount() > 0) {
+//                System.out.println("Wood Amount: " + item.getCount());
+//            } else {
+//                System.out.println("No wood. You must gather wood before repairing"
+//                        + " your wagon");
+//            }
+//        } else {
+//            System.out.println("No wood. You must gather wood before repairing"
+//                    + " your wagon");
+//        }
+//        System.out.println("\nR - Repair Wagon "
+//                + "\nQ - Quit");
+//        String input = this.getInput("Enter your selection: ");
+//        inputs[0] = input;
+//
+//        if (inputs[0].length() < 1) {
+//            System.out.println("You must enter a non-blank value");
+//        }
+//
+//        return inputs;
     }
 
     @Override
-    public boolean doAction(String[] inputs) {
+    public boolean doAction(String inputs) {
 
-        String menuItem = inputs[0].toUpperCase();
+        String menuItem = inputs.toUpperCase();
 
         if ("R".equals(menuItem)) {
             repairWagonMenu();
